0e6309fe7017d6bf4e4bc9c1f5e95ea27c2e484d
reyhanasta <astareyhan@gmail.com>
2026-01-07 11:25:35 +0700

    ahoy

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,23 +1,23 @@
 import skills from "@/data/SkillsList";
-import { Card, CardContent } from "./ui/card";
+import { Card } from "./ui/card";
 import { Link } from "react-router-dom";
 
 export default function Skills() {
   return (
     <section
       id="skills"
-      className="min-h-screen flex items-center justify-center px-4 py-20"
+      className="min-h-screen flex items-center justify-center px-4 py-20 "
     >
       <div className="max-w-4xl mx-auto w-full">
-        <h2 className="text-4xl md:text-5xl font-bold text-center mb-16 md:fade-in">
+        <h2 className="text-4xl md:text-5xl font-bold text-center mb-16 fade-in">
           Skill & Expertise
         </h2>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
           {skills.map((skill) => (
             <Card
               key={skill.name}
-              className="group md:hover:shadow-2xl md:hover:shadow-primary/40 transition-all duration-300 md:hover:-translate-y-1 border-slate-300 bg-white/15 "
+              className="group md:hover:shadow-2xl md:hover:shadow-primary/40 transition-all duration-300 md:hover:-translate-y-1 border-slate-300 bg-white/15 fade-in-delay-1"
             >
               <Link
                 to={skill.url || ""}
